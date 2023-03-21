a66f855f66f8435f20a828b9de6b08f041e86a2a
Surbhi Wahie <surbhiwahie@gmail.com>
2023-03-21 14:07:18 -0400

    new commit

--- week_1_basics_n_setup/2_docker_sql/pipeline.py
+++ week_1_basics_n_setup/2_docker_sql/pipeline.py
@@ -5,6 +5,6 @@ import pandas as pd
 print(sys.argv)
 
 day = sys.argv[1]
-# some fancy stuff
 
-print(f"job finished successfully for day = f{day}")
+
+print(f'job finished successfully for day = f{day}')
