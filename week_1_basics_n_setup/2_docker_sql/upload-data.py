011309412e2229458f4c23e1f779ff5b0bdc0e60
Surbhi Wahie <surbhiwahie@gmail.com>
2023-03-27 10:37:04 -0400

    hello new change

--- week_1_basics_n_setup/2_docker_sql/upload-data.py
+++ week_1_basics_n_setup/2_docker_sql/upload-data.py
@@ -10,4 +10,5 @@ df =pd.read_csv('yellow_tripdata_2021-01.csv', nrows =100)
 df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
 df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
+print("I am making this new change")
 print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
