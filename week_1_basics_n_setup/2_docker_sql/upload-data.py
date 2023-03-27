4f951397a641842886f378dc5bdb5147d9b53ea2
Surbhi Wahie <surbhiwahie@gmail.com>
2023-03-27 10:36:18 -0400

    uploading ny_taxi data

---
+++ week_1_basics_n_setup/2_docker_sql/upload-data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import psycopg2 
+
+engine = create_engine('postgresql://postgres:postgres@localhost:5432/ny_taxi')
+
+print("hello I am here")
+df =pd.read_csv('yellow_tripdata_2021-01.csv', nrows =100)
+
+df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
