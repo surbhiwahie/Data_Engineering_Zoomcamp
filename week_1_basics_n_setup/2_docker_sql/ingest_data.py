685f734a1aec8b54dfb2d7a19aa3e27cb7dc5330
Surbhi Wahie <surbhiwahie@gmail.com>
2023-04-07 16:59:00 -0400

    Add files via upload

--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -1,30 +1,10 @@
+import os
+import argparse
+from time import time
 import pandas as pd
 from sqlalchemy import create_engine
-import psycopg2 
-from time import time
-import argparse
-import os
 
-def  parquet_to_csv(parquet_file,csv_file):
-    df = pd.read_parquet(parquet_file, engine = "pyarrow")
-    df.to_csv(csv_file, index=False)
-    
-def ingest(csv_file, table_name, engine, chunksize=100000):
-    df_iter = pd.read_csv(csv_file, iterator=True, chunksize=chunksize)
-    run = True
-    while run:
-        try:
-            t_start = time()
-            df = next(df_iter)
-            df['tpep_pickup_datetime'] = pd.to_datetime(df['tpep_pickup_datetime'])
-            df['tpep_dropoff_datetime'] = pd.to_datetime(df['tpep_dropoff_datetime'])
-            df.to_sql(name=table_name, con=engine, if_exists='append')
-            t_end = time()
-            print(f'inserted another chunk, took {t_end-t_start:.3f} seconds')
-        except Exception:
-            run = False
-            
-            
+
 def main(params):
     user = params.user
     password = params.password
@@ -33,16 +13,53 @@ def main(params):
     db = params.db
     table_name = params.table_name
     url = params.url
-    parquet_file = 'output.parquet'
-    csv_file = 'output.csv'
-    os.system(f'wget {url} -O {parquet_file}')
-    parquet_to_csv(parquet_file, csv_file)
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
-    engine.connect()
-    ingest(csv_file, table_name, engine)
     
+    # the backup files are gzipped, and it's important to keep the correct extension
+    # for pandas to be able to open the file
+    if url.endswith('.csv.gz'):
+        csv_name = 'output.csv.gz'
+    else:
+        csv_name = 'output.csv'
+
+    os.system(f"wget {url} -O {csv_name}")
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+
+    while True: 
+
+        try:
+            t_start = time()
+            
+            df = next(df_iter)
+
+            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+            df.to_sql(name=table_name, con=engine, if_exists='append')
+
+            t_end = time()
+
+            print('inserted another chunk, took %.3f second' % (t_end - t_start))
+
+        except StopIteration:
+            print("Finished ingesting data into the postgres database")
+            break
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
     parser.add_argument('--user', required=True, help='user name for postgres')
     parser.add_argument('--password', required=True, help='password for postgres')
     parser.add_argument('--host', required=True, help='host for postgres')
@@ -50,5 +67,7 @@ if __name__ == '__main__':
     parser.add_argument('--db', required=True, help='database name for postgres')
     parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
     parser.add_argument('--url', required=True, help='url of the csv file')
+
     args = parser.parse_args()
+
     main(args)
